3876c3c3d6502795ac677c8d99e04d73aaa1284a
wangbo <pmb2020@163.com>
2022-11-06 14:53:54 +0800

    搜索功能完善

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,7 +1,8 @@
 <template>
 	<view class="search">
 		<view class="al-flex" style="padding-top: var(--status-bar-height);">
-			<uni-easyinput class="input" v-model="keyWord" :inputBorder="false" focus trim="both" :styles="styles" prefixIcon="search" placeholder="搜索关键词" @confirm="toSearch()">
+			<uni-easyinput class="input" v-model="keyWord" :inputBorder="false" focus trim="both" :styles="styles"
+				prefixIcon="search" placeholder="搜索关键词" @confirm="toSearch()">
 			</uni-easyinput>
 			<view @click="toSearch" class="search-text">搜索</view>
 		</view>
@@ -14,70 +15,74 @@
 				</view>
 			</view>
 			<view class="body">
-				<text class="al-tag" v-for="(item,index) in historyKeywords" :key="index">{{item}}</text>
-			</view>	
-		</view>	
-		
+				<text @click="tagClick(item)" class="al-tag" v-for="(item,index) in historyKeywords" :key="index">{{item}}</text>
+			</view>
+		</view>
+
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				keyWord: '',
-				styles: {
-					color: '#666',
-					borderColor: '#2979FF',
-					borderRadius: '10px'
-				},
-				historyKeywords:['华伦天奴','运动鞋','苹果13']
+<script setup>
+	import {ref} from 'vue'
+	const keyWord = ref('')
+	const styles = ref({
+		color: '#666',
+		borderColor: '#2979FF',
+		borderRadius: '10px'
+	})
+	const historyKeywords = ref(uni.getStorageSync('historyKeywords') || [])
+	
+	const toSearch = ()=>{
+		historyKeywords.value.push(keyWord.value)
+		uni.setStorageSync('historyKeywords',historyKeywords.value)
+		uni.navigateTo({
+			url: '/pages/goodsList/goodsList?keyWord=' + keyWord.value
+		})
+	}
+	
+	const tagClick = (name)=>{
+		keyWord.value = name
+	}
+	
+	const delHistory = ()=>{
+		uni.showModal({
+			title: '提示',
+			content: '确定要清除搜索历史吗',
+			success: function() {
+				historyKeywords.value = []
+				uni.removeStorageSync('historyKeywords')
 			}
-		},
-		methods: {
-			toSearch() {
-				console.log('开始搜索了'+this.keyWord)
-				uni.navigateTo({
-					url:'/pages/goodsList/goodsList?keyWord='+this.keyWord
-				})
-			},
-			delHistory(){
-				uni.showModal({
-					title:'提示',
-					content:'确定要清除搜索历史吗',
-					success:function(){
-						this.historyKeywords=[]
-						console.log(this.historyKeywords)
-					}
-				})
-			},
-			cancel() {
-				console.log('取消变成搜索')
-				return false
-			}
-		}
+		})
+	}
+	const cancel = ()=>{
+		console.log('取消变成搜索')
+		return false
 	}
 </script>
 
 <style lang="scss" scoped>
-	.search{
+	.search {
 		padding: 30rpx;
 		font-size: 28rpx;
 		background-color: #fff;
 		height: 100vh;
-		.input{
+
+		.input {
 			border: 1px solid #ccc;
 			padding: 0 10px;
 			border-radius: 20px;
 		}
 	}
-	.search-history{
+
+	.search-history {
 		margin-top: 30rpx;
-		.body{
+
+		.body {
 			margin-top: 28rpx;
 		}
 	}
-	.search-text{
+
+	.search-text {
 		margin-left: 10rpx;
 	}
 </style>
